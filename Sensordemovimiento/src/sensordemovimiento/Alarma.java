4f4ccd2e4a381f89941b94cbcb3e46aaba59a6cd
diegogj2024 <diegogarcia0809@outlook.com>
2025-02-28 09:32:20 -0500

    Add files via upload

---
+++ Sensordemovimiento/src/sensordemovimiento/Alarma.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sensordemovimiento;
+
+/**
+ *
+ * @author SENA
+ */
+public class Alarma {
+    public Alarma(){
+        
+    }
+    
+    public static void PrenderAlarma(){
+        System.out.println("bi bi bi bi bi bi bi bi!!!");
+    }
+}
---
+++ Sensordemovimiento/src/sensordemovimiento/Sensordemovimiento.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sensordemovimiento;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Sensordemovimiento {
+    public static Random random = new Random();
+    public static int numeroAleatorio = random.nextInt(2) + 1;
+    public static int numerosensor=random.nextInt(2)+1;
+    public static String tiempo;
+    
+    public Sensordemovimiento(){
+        switch (Sensordemovimiento.numeroAleatorio) {
+            case 1:
+                Sensordemovimiento.tiempo="noche";
+                System.out.println("1");
+                break;
+            case 2:
+                Sensordemovimiento.tiempo="dia";
+                System.out.println("2");
+                break;
+            default:
+                throw new AssertionError();
+        }
+        Sensordemovimiento.validarsensor();
+    }
+    
+    public static void validarsensor(){
+        int avisos=0;
+        for (int i=1;i<=3;i++){
+            int numerosensor=random.nextInt(2)+1;
+            if (numerosensor==1 && Sensordemovimiento.tiempo.equals("noche")){
+                System.out.println(".");
+                avisos+=1;
+            }else if(numerosensor!=1 || !Sensordemovimiento.tiempo.equals("noche")){
+                System.out.println("no se ha detectado nada");
+            }
+        }
+        
+        if(avisos>=2){
+            Alarma.PrenderAlarma();
+        }
+    }
+}
