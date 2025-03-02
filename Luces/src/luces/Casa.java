38dd0de53a537bc23f594b82939a8d8856430a35
diegogj2024 <diegogarcia0809@outlook.com>
2025-03-02 16:10:46 -0500

    Add files via upload

---
+++ Luces/src/luces/Casa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luces;
+
+public class Casa {
+    
+    public Casa(){
+        
+    }
+    
+    public static void Comprobarluces(){
+        Sensorluces.Validarmovimiento();
+    }
+}
